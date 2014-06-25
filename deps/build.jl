5734c547120b02e71e389c9a4b13ee3ef7c61ab0
Mike Nolta <mike@nolta.net>
2014-06-25 17:15:51 -0400

    fix spurious darwin build error

--- deps/build.jl
+++ deps/build.jl
@@ -9,12 +9,14 @@ provides(Sources, URI(url), libcfitsio, unpacked_dir="cfitsio")
 depsdir = BinDeps.depsdir(libcfitsio)
 srcdir = joinpath(depsdir, "src", "cfitsio")
 prefix = joinpath(depsdir, "usr")
+@unix_only libfilename = "libcfitsio.so"
+@osx_only libfilename = "libcfitsio.dylib"
 provides(BuildProcess,
          (@build_steps begin
             GetSources(libcfitsio)
             @build_steps begin
                 ChangeDirectory(srcdir)
-                FileRule(joinpath(prefix,"lib","libcfitsio.so"),
+                FileRule(joinpath(prefix,"lib",libfilename),
                          @build_steps begin
                          `./configure --prefix=$prefix`
                          `make shared install`

62afc0c7b5eb39a0abe28b19a92dd1ed0f126128
siddharthlal25 <siddharthlal25@gmail.com>
2020-08-03 07:37:47 +0530

    added get_default_header and tests

--- src/header.jl
+++ src/header.jl
@@ -422,3 +422,33 @@ function show(io::IO, hdr::FITSHeader)
         i != n && println(io)
     end
 end
+
+"""
+    get_default_header(data::AbstractArray)
+
+Creates a default header from an `AbstractArray` with fields being `SIMPLE`, `BITPIX`, `NAXIS`, axis lengths and `EXTEND`.
+"""
+function get_default_header(data::AbstractArray{T}) where T <: Number
+    # assigning keys
+    hdu_keys = ["SIMPLE",
+                "BITPIX",
+                "NAXIS",
+                [string(Symbol("NAXIS", i)) for i in 1:ndims(data)]...,
+                "EXTEND"]
+
+    # assiging values
+    hdu_values = [true,                                           # SIMPLE
+                  FITSIO.Libcfitsio.bitpix_from_type(T),          # BITPIX
+                  ndims(data),                                    # NAXIS
+                  reverse(size(data))...,                         # size of each axis
+                  true]                                           # EXTEND
+
+    # assigning comments
+    comments = ["file does conform to FITS standard",                                   # comment for SIMPLE
+                "number of bits per data pixel",                                        # comment for BITPIX
+                "number of data axes",                                                  # comment for NAXIS
+                [string(Symbol("length of data axis ", i)) for i in 1:ndims(data)]...,  # comments for axis length
+                "FITS dataset may contain extensions"]                                  # comment for EXTEND
+
+    return FITSHeader(hdu_keys, hdu_values, comments)
+end

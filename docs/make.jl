246fc4df8808a4e8f16fec7f07afad6666031cd2
Abhro R <5664668+abhro@users.noreply.github.com>
2024-06-12 15:46:50 -0400

    Use newer stable version of Documenter.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,8 @@ makedocs(
     modules = [FITSIO],
     sitename = "FITSIO.jl",
     format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
-    pages = pages
+    pages = pages,
+    checkdocs = :exports,
 )
 
 deploydocs(repo = "github.com/JuliaAstro/FITSIO.jl.git", push_preview=true)

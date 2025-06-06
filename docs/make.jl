2371912ce1f8097078c4d2ddee8f92ab8355976b
Ian Weaver <weaveric@gmail.com>
2025-06-06 16:09:44 -0700

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,18 @@
 using Documenter, FITSIO
 
 include("pages.jl")
-makedocs(
+makedocs(;
     modules = [FITSIO],
     sitename = "FITSIO.jl",
     format = Documenter.HTML(
         prettyurls = get(ENV, "CI", nothing) == "true",
-        canonical = "https://juliaastro.org/FITSIO/stable",
+        canonical = "https://juliaastro.org/FITSIO/stable/",
     ),
     pages = pages,
     checkdocs = :exports,
 )
 
-deploydocs(
+deploydocs(;
     repo = "github.com/JuliaAstro/FITSIO.jl.git",
     push_preview = true,
     versions = ["stable" => "v^", "v#.#"], # Restrict to minor releases
